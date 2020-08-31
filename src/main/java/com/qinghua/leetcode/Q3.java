d51218631ed159fa4f07b62fe0c10aa1ecc967a5
otis <liqinghua233@gmail.com>
2020-08-31 17:58:55 +0800

    【20200831】Q3未完成

---
+++ src/main/java/com/qinghua/leetcode/Q3.java
@@ -0,0 +1,24 @@
+package com.qinghua.leetcode;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ */
+public class Q3 {
+    public static void main(String[] args) {
+        System.out.println("saaa".charAt(0));
+    }
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //暴力解法 每一个位置做第一个字母,分别和下面的字母组合
+        int max = 0;
+        int i = 0;
+        while (i < s.length()) {
+
+        }
+    }
+}
